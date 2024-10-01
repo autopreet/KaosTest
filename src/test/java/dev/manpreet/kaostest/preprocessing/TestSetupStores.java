add52df2786a3a474854fc2e6408579040bd2af4
Manpreet <manpreet.333@gmail.com>
2024-10-01 12:00:33 +0530

    Fixed unit tests for store setup

--- src/test/java/dev/manpreet/kaostest/preprocessing/TestSetupStores.java
+++ src/test/java/dev/manpreet/kaostest/preprocessing/TestSetupStores.java
@@ -8,13 +8,16 @@ import dev.manpreet.kaostest.stores.TestClassData;
 import dev.manpreet.kaostest.stores.TestMethodData;
 import dev.manpreet.kaostest.stores.TestPackageData;
 import dev.manpreet.kaostest.util.SuiteXMLUtils;
+import dev.manpreet.testutils.TestUtils;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
@@ -31,121 +34,120 @@ public class TestSetupStores {
         store.clear();
     }
 
-//    @Test
-//    public void testStoreSetupWithClassesAndPackagesInSuite() {
-//        updateStore(Paths.get(resourcesPath, "testsuite_classes_packages.xml").toString());
-//        assertListeners();
-//        assertStore();
-//    }
-//
-//    @Test
-//    public void testStoreSetupWithClassesOnlyInSuite() {
-//        updateStore(Paths.get(resourcesPath, "testsuite_classes.xml").toString());
-//        assertListeners();
-//        assertStore();
-//    }
-//
-//    @Test
-//    public void testStoreSetupWithPackagesOnlyInSuite() {
-//        updateStore(Paths.get(resourcesPath, "testsuite_packages.xml").toString());
-//        assertListeners();
-//        assertStore();
-//    }
-//
-//    @Test
-//    public void testStoreSetupWithClassesOnlyInOneTestSuite() {
-//        updateStore(Paths.get(resourcesPath, "testsuite_singletest_classes.xml").toString());
-//        assertListeners();
-//        assertStore();
-//    }
-//
-//    @Test
-//    public void testStoreSetupWithPackagesOnlyInOneTestSuite() {
-//        updateStore(Paths.get(resourcesPath, "testsuite_singletest_packages.xml").toString());
-//        assertListeners();
-//        assertStore();
-//    }
-//
-//    private void updateStore(String suiteXMLPath) {
-//        suite = SuiteXMLUtils.deserializeSuiteXML(suiteXMLPath);
-//        SuiteXMLUtils.validateSuite(suite);
-//        List<SuiteClass> testClasses = suite.getAllTestClasses();
-//        List<SuitePackage> testPackages = suite.getAllTestPackages();
-//        testClasses.forEach(eachClass -> setupStores.addTests(eachClass.getName(), true));
-//        testPackages.forEach(eachPackage -> setupStores.addTests(eachPackage.getName(), false));
-//    }
-//
-//    private void assertListeners() {
-//        assertNotNull(suite.getListener());
-//        assertEquals(suite.getListener().size(), 2);
-//        Set<String> expectedListeners = new HashSet<>();
-//        expectedListeners.add("dev.manpreet.demotests.listeners.TestListener");
-//        expectedListeners.add("dev.manpreet.demotests.listeners.AnotherListener");
-//        assertTrue(expectedListeners.contains(suite.getListener().get(0).getClassName()));
-//        assertTrue(expectedListeners.contains(suite.getListener().get(1).getClassName()));
-//    }
-//
-//    private void assertStore() {
-//        assertEquals(store.getName(), "ALL");
-//        assertNotNull(store.getPackagesData());
-//        System.out.println(store.getPackagesData());
-//        //Reflections library ends up loading the current tests in this class also as it scans current classloader
-//        removeInternalPackages();
-//        System.out.println(store.getPackagesData().values().stream().map(TestPackageData::getName));
-//        assertEquals(store.getPackagesData().size(), 2);
-//
-//        TestPackageData demoTestPkg = assertPackageInStore("dev.manpreet.demotests");
-//        assertNotNull(demoTestPkg.getTestClassesData());
-//        assertEquals(demoTestPkg.getTestClassesData().size(), 2);
-//
-//        TestClassData tClassAData = assertClassInPackageData(demoTestPkg, "TestClassA");
-//        assertMethodInClassData(tClassAData, "runPassingTest1");
-//        assertMethodInClassData(tClassAData, "runPassingTest2");
-//        TestClassData tClassBData = assertClassInPackageData(demoTestPkg, "TestClassB");
-//        assertMethodInClassData(tClassBData, "runPassingTest1");
-//        assertMethodInClassData(tClassBData, "runFailingTest2");
-//        assertMethodInClassData(tClassBData, "runSkippingTest3");
-//
-//        TestPackageData moreTestPkg = assertPackageInStore("dev.manpreet.demotests.moretests");
-//        assertNotNull(moreTestPkg.getTestClassesData());
-//        assertEquals(moreTestPkg.getTestClassesData().size(), 1);
-//
-//        TestClassData tClassCData = assertClassInPackageData(moreTestPkg, "TestClassC");
-//        assertMethodInClassData(tClassCData, "runPassingTest1");
-//        assertMethodInClassData(tClassCData, "runPassingTest2");
-//    }
-//
-//    private TestPackageData assertPackageInStore(String packageName) {
-//        TestPackageData testPackageData = store.getPackagesData().get(packageName);
-//        assertNotNull(testPackageData);
-//        assertEquals(testPackageData.getName(), packageName);
-//        assertNotNull(testPackageData.getSubscriber());
-//        assertEquals(testPackageData.getSubscriber(), store);
-//        return testPackageData;
-//    }
-//
-//    private TestClassData assertClassInPackageData(TestPackageData testPackageData, String className) {
-//        TestClassData testClassData = testPackageData.getTestClassesData().get(className);
-//        assertNotNull(testClassData);
-//        assertEquals(testClassData.getName(), className);
-//        assertNotNull(testClassData.getSubscriber());
-//        assertEquals(testClassData.getSubscriber(), testPackageData);
-//        return testClassData;
-//    }
-//
-//    private void assertMethodInClassData(TestClassData testClassData, String methodName) {
-//        TestMethodData testMethodData = testClassData.getTestMethodsData().get(methodName);
-//        assertNotNull(testMethodData);
-//        assertEquals(testMethodData.getName(), methodName);
-//        assertNotNull(testMethodData.getSubscriber());
-//        assertEquals(testMethodData.getSubscriber(), testClassData);
-//        assertNotNull(testMethodData.getInstancesData());
-//    }
-//
-//    private void removeInternalPackages() {
-//        store.getPackagesData().remove("dev.manpreet.kaostest.preprocessing");
-//        store.getPackagesData().remove("dev.manpreet.kaostest.providers.threadcount");
-//        store.getPackagesData().remove("dev.manpreet.kaostest.providers.duration");
-//        store.getPackagesData().remove("dev.manpreet.kaostest.providers.testorder");
-//    }
+    @Test
+    public void testStoreSetupWithClassesAndPackagesInSuite() {
+        updateStore(TestUtils.getFilePath("testsuite_classes_packages.xml"));
+        assertListeners();
+        assertStore();
+    }
+
+    @Test
+    public void testStoreSetupWithClassesOnlyInSuite() {
+        updateStore(TestUtils.getFilePath("testsuite_classes.xml"));
+        assertListeners();
+        assertStore();
+    }
+
+    @Test
+    public void testStoreSetupWithPackagesOnlyInSuite() {
+        updateStore(TestUtils.getFilePath("testsuite_packages.xml"));
+        assertListeners();
+        assertStore();
+    }
+
+    @Test
+    public void testStoreSetupWithClassesOnlyInOneTestSuite() {
+        updateStore(TestUtils.getFilePath("testsuite_singletest_classes.xml"));
+        assertListeners();
+        assertStore();
+    }
+
+    @Test
+    public void testStoreSetupWithPackagesOnlyInOneTestSuite() {
+        updateStore(TestUtils.getFilePath("testsuite_singletest_packages.xml"));
+        assertListeners();
+        assertStore();
+    }
+
+    private void updateStore(String suiteXMLPath) {
+        suite = SuiteXMLUtils.deserializeSuiteXML(suiteXMLPath);
+        SuiteXMLUtils.validateSuite(suite);
+        List<SuiteClass> testClasses = suite.getAllTestClasses();
+        List<SuitePackage> testPackages = suite.getAllTestPackages();
+        testClasses.forEach(eachClass -> setupStores.addTests(eachClass.getName(), true));
+        testPackages.forEach(eachPackage -> setupStores.addTests(eachPackage.getName(), false));
+    }
+
+    private void assertListeners() {
+        assertNotNull(suite.getListener());
+        assertEquals(suite.getListener().size(), 2);
+        Set<String> expectedListeners = new HashSet<>();
+        expectedListeners.add("dev.manpreet.demotests.listeners.TestListener");
+        expectedListeners.add("dev.manpreet.demotests.listeners.AnotherListener");
+        assertTrue(expectedListeners.contains(suite.getListener().get(0).getClassName()));
+        assertTrue(expectedListeners.contains(suite.getListener().get(1).getClassName()));
+    }
+
+    private void assertStore() {
+        assertEquals(store.getName(), "ALL");
+        assertNotNull(store.getPackagesData());
+        System.out.println(store.getPackagesData());
+        //Reflections library ends up loading the current tests in this class also as it scans current classloader
+        removeInternalPackages();
+        System.out.println(store.getPackagesData().values().stream().map(TestPackageData::getName));
+        assertEquals(store.getPackagesData().size(), 2);
+
+        TestPackageData demoTestPkg = assertPackageInStore("dev.manpreet.demotests");
+        assertNotNull(demoTestPkg.getTestClassesData());
+        assertEquals(demoTestPkg.getTestClassesData().size(), 2);
+
+        TestClassData tClassAData = assertClassInPackageData(demoTestPkg, "TestClassA");
+        assertMethodInClassData(tClassAData, "runPassingTest1");
+        assertMethodInClassData(tClassAData, "runPassingTest2");
+        TestClassData tClassBData = assertClassInPackageData(demoTestPkg, "TestClassB");
+        assertMethodInClassData(tClassBData, "runPassingTest1");
+        assertMethodInClassData(tClassBData, "runFailingTest2");
+        assertMethodInClassData(tClassBData, "runSkippingTest3");
+
+        TestPackageData moreTestPkg = assertPackageInStore("dev.manpreet.demotests.moretests");
+        assertNotNull(moreTestPkg.getTestClassesData());
+        assertEquals(moreTestPkg.getTestClassesData().size(), 1);
+
+        TestClassData tClassCData = assertClassInPackageData(moreTestPkg, "TestClassC");
+        assertMethodInClassData(tClassCData, "runPassingTest1");
+        assertMethodInClassData(tClassCData, "runPassingTest2");
+    }
+
+    private TestPackageData assertPackageInStore(String packageName) {
+        TestPackageData testPackageData = store.getPackagesData().get(packageName);
+        assertNotNull(testPackageData);
+        assertEquals(testPackageData.getName(), packageName);
+        assertNotNull(testPackageData.getSubscriber());
+        assertEquals(testPackageData.getSubscriber(), store);
+        return testPackageData;
+    }
+
+    private TestClassData assertClassInPackageData(TestPackageData testPackageData, String className) {
+        TestClassData testClassData = testPackageData.getTestClassesData().get(className);
+        assertNotNull(testClassData);
+        assertEquals(testClassData.getName(), className);
+        assertNotNull(testClassData.getSubscriber());
+        assertEquals(testClassData.getSubscriber(), testPackageData);
+        return testClassData;
+    }
+
+    private void assertMethodInClassData(TestClassData testClassData, String methodName) {
+        TestMethodData testMethodData = testClassData.getTestMethodsData().get(methodName);
+        assertNotNull(testMethodData);
+        assertEquals(testMethodData.getName(), methodName);
+        assertNotNull(testMethodData.getSubscriber());
+        assertEquals(testMethodData.getSubscriber(), testClassData);
+        assertNotNull(testMethodData.getInstancesData());
+    }
+
+    private void removeInternalPackages() {
+        Set<String> internalPackages = store.getPackagesData().keySet().stream()
+                .filter(eachPkg -> eachPkg.contains("kaostest")).collect(Collectors.toSet());
+        internalPackages.forEach(eachPkg -> store.getPackagesData().remove(eachPkg));
+    }
 }
